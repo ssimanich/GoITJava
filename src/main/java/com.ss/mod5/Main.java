adb49a7dcd9db12d00381b445e9d669b838f762b
Stas <ssimanich@rambler.ru>
2015-12-27 23:38:05 +0200

    dekrement/increment

--- src/main/java/com.ss/mod5/Main.java
+++ src/main/java/com.ss/mod5/Main.java
@@ -2,13 +2,10 @@ package com.ss.mod5;
 
 import java.util.Arrays;
 
-/**
- * Created by ss on 27.12.2015.
- */
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {2,-1,5,3,1};
-        int[] sort = QuickSort.sort(arr, 0, 3);
+        int[] mass = {2,-1,3,1};
+        int[] sort = QuickSort.sort(mass, 0, 3);
         System.out.println(Arrays.toString(sort));
     }
 }
