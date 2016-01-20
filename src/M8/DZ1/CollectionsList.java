b963946f975377b8d87713b30416a41c2119a49c
Stas <ssimanich@rambler.ru>
2016-01-20 23:44:22 +0200

    исправления

---
+++ src/M8/DZ1/CollectionsList.java
@@ -0,0 +1,13 @@
+package M8.DZ1;
+import java.util.ArrayList;
+public class CollectionsList {
+    public static void main(String[] args) {
+        ArrayList<String> flowersmass = new ArrayList();
+        String[] flowersMass = {"Aster","Bouquet","Chamomile","Flower","Rose","Rosebush","Tultip"};
+        for (int i = 0; i < flowersMass.length; i++) {
+            flowersmass.add(flowersMass[i]);
+            String res = flowersmass.get(i);
+            System.out.println(res);
+        }
+    }
+}
