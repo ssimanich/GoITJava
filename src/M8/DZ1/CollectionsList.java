835cc5ff817b54a8df7489323ff38810f23284bb
Stas <ssimanich@rambler.ru>
2016-01-21 15:58:57 +0200

    было непонятно как отрабатывает сортировка так как в масив я ввел уже отсортированый список файлов

--- src/M8/DZ1/CollectionsList.java
+++ src/M8/DZ1/CollectionsList.java
@@ -6,13 +6,10 @@ import java.util.TreeSet;
 public class CollectionsList {
     public static void main(String[] args) {
         ArrayList<String> flowersmass = new ArrayList<String>();
-        String[] flowersMass = {"Tultip","Aster","Bouquet","Chamomile","Flower","Rose","Rosebush","Tultip"};
+        String[] flowersMass = {"Aster","Bouquet","Chamomile","Flower","Rose","Rosebush","Tultip"};
         for (int i = 0; i < flowersMass.length; i++) {
             flowersmass.add(flowersMass[i]);}
         System.out.println(flowersmass);
-        SortedSet<String> set = new TreeSet<String>(flowersmass);
-        System.out.print(set);
-
 
     }
 }
