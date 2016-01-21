f39f8ca767048ea267a547cbb55ec83533cb622b
Stas <ssimanich@rambler.ru>
2016-01-21 23:46:34 +0200

    исправления

---
+++ src/M8/DZ2/Comp.java
@@ -0,0 +1,18 @@
+package M8.DZ2;
+
+class Comp implements Comparable {
+    String str;
+    Comp(String str) {
+        this.str = str;
+    }
+    @Override
+    public int compareTo(Object obj) {
+        Comp entry = (Comp) obj;
+        int result = str.compareTo(entry.str);
+        if(result != 0) {
+            return result;
+        }
+        return 0;
+    }
+
+}
