8772df67824913a06f355c8eece514fcfeaca2dd
Stas <ssimanich@rambler.ru>
2016-01-25 00:03:55 +0200

    исправления

--- src/M9/Cesar.java
+++ src/M9/Cesar.java
@@ -35,9 +35,9 @@ public class Cesar{
             }
         }
         System.out.print("\n"+"Зашифрованное текстовое представление коллекци обьектов из " +
-                " из Модуля 3: ООП в Java:\n"+encryption+"\n");
+                " из Модуля 3: ООП в Java:\n"+encryption.toString ()+"\n");
         System.out.print("\n"+"Расшифрованое текстовое представление коллекци обьектов из " +
-                " из Модуля 3: ООП в Java:\n"+decryption+"\n");
+                " из Модуля 3: ООП в Java:\n"+decryption.toString ()+"\n");
         System.out.print("\n"+"Конец выполнения программы!");
     }
 }
