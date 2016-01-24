137201c8c619d7e399d096645f7834848bb65b64
Stas <ssimanich@rambler.ru>
2016-01-24 23:19:46 +0200

    исправления

--- src/M9/Cesar.java
+++ src/M9/Cesar.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Cesar {
     public static void main(String[] args){
         String message =  "Flower, Tultip, Aster, Bouquet, Chamomile, Rose, Rosebush";
-        System.out.println("Enter the pitch");
+        System.out.println ("Текстовое представление коллекци обьектов из ДЗ из Модуля 3: ООП в Java:\n"+message+"\n");
+        System.out.print("Введите ключ, для шифрования методом Цезаря:");
         Scanner scanner = new Scanner (System.in);
         final int pitch= (Integer.parseInt (scanner.next()));
         StringBuilder encryption = new StringBuilder();
@@ -33,7 +34,10 @@ public class Cesar {
                 decryption.append(i);
             }
         }
-        System.out.println (encryption);
-        System.out.println (decryption);
+        System.out.println ("\n"+"Зашифрованное текстовое представление коллекци обьектов из " +
+                " из Модуля 3: ООП в Java:\n"+encryption+"\n");
+        System.out.println ("Расшифрованое текстовое представление коллекци обьектов из " +
+                " из Модуля 3: ООП в Java:\n"+decryption+"\n");
+        System.out.println("Конец выполнения программы");
     }
 }
