ab9024fb6774cb527f54cdb5e7c9a368b1462321
Stas <ssimanich@rambler.ru>
2016-01-24 16:03:53 +0200

    исправления

--- src/M1/DZ1.java
+++ src/M1/DZ1.java
@@ -1,3 +1,5 @@
+package M1;
+
 public class DZ1
 {
 public static void main(String[] args)
---
+++ src/M8/DZ2v2/SortedFlowersNoCollectionsSort.java
@@ -0,0 +1,19 @@
+package M8.DZ2v2;
+
+import java.util.*;
+public class SortedFlowersNoCollectionsSort {
+
+    public static void main(String[] args) {
+        TreeSet<String> flowersmass = new TreeSet<String>();
+        flowersmass.add("Flower");
+        flowersmass.add("Tultip");
+        flowersmass.add("Bouquet");
+        flowersmass.add("Chamomile");
+        flowersmass.add("Rose");
+        flowersmass.add("Rosebush");
+        flowersmass.add("Aster");
+        System.out.print(flowersmass);
+
+    }
+}
+//чуток долго тупил так как файди в системе тоже были отсортированы по имени
