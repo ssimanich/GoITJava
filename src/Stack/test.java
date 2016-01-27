fd984a632be237375e754eb3e77ad0714ee2ac6a
Stas <ssimanich@rambler.ru>
2016-01-27 17:18:28 +0200

    Project Fifj&Lifo

--- src/Stack/test.java
+++ src/Stack/test.java
@@ -2,21 +2,22 @@ package Stack;
 import java.util.Scanner;
 public class test{
     public static void main(String[] args) {
-        int[] a;
-        int n,option;
-        Scanner in = new Scanner(System.in);
-        System.out.print("Введите размерность массива:\t");
-        n = in.nextInt();
-        a = new int[n];
-        Scanner selection = new Scanner(System.in);
-        System.out.print("\nСделайте выбор:\n"+"\t1. Добавление данных\n"+"\t2. Обработка очереди\n"+"\t3. size\n");
-        option = selection.nextInt();
-        switch (option){
+    int[] a;
+    int n,option;
+    Scanner in = new Scanner(System.in);
+    System.out.print("Введите размерность массива:\t");
+    n = in.nextInt();
+    a = new int[n];
+    boolean isClose = false;
+    while (!isClose) {
+        System.out.print("\nСделайте выбор:\n" + "\t1. Добавление данных\n" + "\t2. Обработка очереди\n" + "\t3. size\n"+ "\t4. exit\n");
+        option = in.nextInt();
+        switch (option) {
             case (1):
                 for (int i = 0; i < n; i++) {
                     System.out.print((i + 1) + "\tэлемент:\t");
                     a[i] = in.nextInt();
-                    }
+                }
                 System.out.print("\nБыл создан массив: \t");
                 for (int i = 0; i < a.length; i++)
                     System.out.print(a[i] + ", ");
@@ -24,14 +25,21 @@ public class test{
             case (2):
                 for (int i = 0; i < a.length; i++) {
 
-                System.out.print("\nУдаление елемента " + (i + 1) + " который поступил в очередь \"" + a[i] + "\"\t. Массив: ");
-                for (int j = i+1; j < a.length; j++)
-                    System.out.print(a[j] + " ");}
+                    System.out.print("\nУдаление елемента " + (i + 1) + " который поступил в очередь \"" + a[i] + "\"\t. Массив: ");
+                    for (int j = i + 1; j < a.length; j++)
+                        System.out.print(a[j] + " ");
+                }
                 break;
-            case (3):int i=0;
-                while( i<a.length){
-                    i=i+1;}
-                System.out.println("Число элементов в структуре\t"+i);
+            case (3):
+                int i = 0;
+                while (i < a.length) {
+                    i = i + 1;
+                }
+                System.out.println("Число элементов в структуре\t" + i);
+                break;
+            case (4):
+                isClose = true;
+                break;
         }
     }
-}
+}}
