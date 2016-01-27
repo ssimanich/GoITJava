3b01ef0bd6429649e43a412440307a698d73b1a4
Stas <ssimanich@rambler.ru>
2016-01-27 17:03:21 +0200

    Project Fifj&Lifo

--- src/FifoLifo/Lifo.java
+++ src/FifoLifo/Lifo.java
@@ -1,17 +1,18 @@
 package FifoLifo;
 
+import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.LinkedList;
+
 
 public class Lifo {
     public static void main(String[] args){
         String[] data={"one","two","three","four","five","six","seven","eight","nine","ten"};
-        Deque<String> list = new LinkedList<String>();
+        Deque<String> list = new ArrayDeque<>();
         for(int i=0;i<data.length;i++) {
             list.addFirst(data[i]);
             System.out.println(list);
         }
-      System.out.println(list+"\n");
+      System.out.println("\n"+list);
         while (!list.isEmpty()){
             list.poll();
             System.out.println(list);}
---
+++ src/Stack/test.java
@@ -0,0 +1,37 @@
+package Stack;
+import java.util.Scanner;
+public class test {
+    public static void main(String[] args) {
+        int[] a;
+        int n,option;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите размерность массива:\t");
+        n = in.nextInt();
+        a = new int[n];
+        Scanner selection = new Scanner(System.in);
+        System.out.print("\nСделайте выбор:\n"+"\t1. Добавление данных\n"+"\t2. Обработка очереди\n"+"\t3. size\n");
+        option = selection.nextInt();
+        switch (option){
+            case (1):
+                for (int i = 0; i < n; i++) {
+                    System.out.print((i + 1) + "\tэлемент:\t");
+                    a[i] = in.nextInt();
+                    }
+                System.out.print("\nБыл создан массив: \t");
+                for (int i = 0; i < a.length; i++)
+                    System.out.print(a[i] + ", ");
+                break;
+            case (2):
+                for (int i = 0; i < a.length; i++) {
+
+                System.out.print("\nУдаление елемента " + (i + 1) + " который поступил в очередь \"" + a[i] + "\"\t. Массив: ");
+                for (int j = i+1; j < a.length; j++)
+                    System.out.print(a[j] + " ");}
+                break;
+            case (3):int i=0;
+                while( i<a.length){
+                    i=i+1;}
+                System.out.println("Число элементов в структуре\t"+i);
+        }
+    }
+}
