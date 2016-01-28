4020ed48d68d20310ca413048f80b7ce7a676c70
Stas <ssimanich@rambler.ru>
2016-01-28 14:10:26 +0200

    исправления 28,01,2016

--- src/FifoLifo/Fifo.java
+++ src/FifoLifo/Fifo.java
@@ -1,15 +1,83 @@
 package FifoLifo;
 
-import java.util.*;
-
 public class Fifo {
-    public static void main(String[] args){
-        String[] data={"one","two","three","four","five","six","seven","eight","nine","ten"};
-        Queue<String> list = new LinkedList<>();
-        Collections.addAll(list, data);
-        System.out.println(list+"\n");
-        while (!list.isEmpty()){
-            list.poll();
-            System.out.println(list);}
+    String[] arr;
+    public Fifo(String[] arr){
+        this.arr = arr;
+    }
+//------------------------------------------------Вывод данных пользователю---------------------------------------------
+    public void print(){
+        for (String anArr : arr) {
+            System.out.println(anArr);
+        }
+    }
+//-----------------------------------------Определение количества элементов в очереди-----------------------------------
+    public int size(){
+        int elements = 0;
+        for (String anArr : arr) {
+            if (anArr != null) {
+                elements++;
+            }
+        }
+        return elements;
+    }
+//---------------------------------------------Удаляет последний добавленый элемент------------------------------------
+    public String pop(){
+        String firstElement = arr[0];
+        for (int i = 0; i<arr.length; i++){
+            if(i!=arr.length-1){
+                arr[i] = arr[i+1];
+                }
+            else arr[i]=null;
+            }
+        return firstElement;
+    }
+//------------------------------Возвращает элемент, находящийся в голове очереди, не удаляя его-------------------------
+    public String front(){
+        String firstElement = null;
+        for (int i=0;i<arr.length; i++){
+            if (arr[i] != null) {
+                firstElement = arr[i];
+                break;
+            }
+        }
+        return firstElement;
+    }
+//------------------------------Добавление элемента в массив и расширяет массив при переполнении------------------------
+    public void push(String element){
+        if (size() < arr.length) {
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == null) {
+                    arr[i] = element;
+                    break;
+                }
+            }
+        } else {
+            String[] arrNew = new String[(int) (arr.length * 2)];
+            for (int i = 0; i < arrNew.length; i++) {
+                if (i < arr.length) {
+                    arrNew[i] = arr[i];
+                }
+                if (arrNew[i] == null) {
+                    arrNew[i] = element;
+                    arr = arrNew;
+                    break;
+                }
+            }
+        }
+    }
+//----------------------------------Метод для предотвращениея отображения "null"--------------------------------------------------------------
+    public void clear() {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == null) {
+                break;
+            } else {
+                arr[i] = null;
+            }
+        }
+    }
+//------------------------------Указывает, есть ли элементы(FALSE) или нет (TRUE) в стеке-------------------------------
+    public boolean empty() {
+        return size() != 0;
     }
 }
--- src/FifoLifo/Lifo.java
+++ src/FifoLifo/Lifo.java
@@ -1,20 +1,84 @@
 package FifoLifo;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-
-
 public class Lifo {
-    public static void main(String[] args){
-        String[] data={"one","two","three","four","five","six","seven","eight","nine","ten"};
-        Deque<String> list = new ArrayDeque<>();
-        for(int i=0;i<data.length;i++) {
-            list.addFirst(data[i]);
-            System.out.println(list);
+    String[] arr;
+    public Lifo(String[] arr){
+        this.arr = arr;
         }
-      System.out.println("\n"+list);
-        while (!list.isEmpty()){
-            list.poll();
-            System.out.println(list);}
+//------------------------------------------------Вывод данных пользователю---------------------------------------------
+    public void print(){
+        for (String anArr : arr) {
+            System.out.println(anArr);
+            }
+        }
+//-----------------------------------------Определение количества элементов в очереди-----------------------------------
+    public int size(){
+        int elements = 0;
+        for (String anArr : arr) {
+            if (anArr != null) {
+                elements++;
+            }
+        }
+        return elements;
+    }
+//---------------------------------------------Удаляет последний добавленый элемент------------------------------------
+    public String pop(){
+        String lastElement = null;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if(arr[i] != null){
+                lastElement = arr[i];
+                arr[i] = null;
+                break;
+            }
+        }
+        return lastElement;
+    }
+//------------------------------Возвращает элемент, находящийся в голове очереди, не удаляя его-------------------------
+    public String front(){
+        String lastElement = null;
+        for (int i = arr.length - 1; i >= 0; i--){
+            if (arr[i] != null) {
+                lastElement = arr[i];
+                break;
+            }
+        }
+        return lastElement;
+    }
+//------------------------------Добавление элемента в массив и расширяет массив при переполнении------------------------
+    public void push(String element){
+        if (size() < arr.length) {
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == null) {
+                    arr[i] = element;
+                    break;
+                }
+            }
+        } else {
+            String[] arrNew = new String[(int) (arr.length * 2)];
+            for (int i = 0; i < arrNew.length; i++) {
+                if (i < arr.length) {
+                    arrNew[i] = arr[i];
+                }
+                if (arrNew[i] == null) {
+                    arrNew[i] = element;
+                    arr = arrNew;
+                    break;
+                }
+            }
+        }
+    }
+//----------------------------------------------------------------------------------------------------------------------
+    public void clear() {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == null) {
+                break;
+            } else {
+                arr[i] = null;
+            }
+        }
+    }//Метод для предотвращениея отображения "null"
+//------------------------------Указывает, есть ли элементы(FALSE) или нет (TRUE) в стеке-------------------------------
+    public boolean empty() {
+        return size() != 0;
     }
 }
