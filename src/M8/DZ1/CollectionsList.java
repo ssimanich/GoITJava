5fa59e9abe54dfa596178fdec86ccbddc5a434bb
Stas <ssimanich@rambler.ru>
2016-01-28 11:04:30 +0200

    исправления 28,01,2016

--- src/M8/DZ1/CollectionsList.java
+++ src/M8/DZ1/CollectionsList.java
@@ -6,8 +6,6 @@ public class CollectionsList{
     public static void main(String[] args) {
         List<Object> flowersmass = new ArrayList();
         String[] flowersMass = { "Flower", "Tultip", "Aster", "Bouquet", "Chamomile", "Rose", "Rosebush"};
-        String[] directoryMass = { "AudioFile", "Directory", "File", "ImageFile", "TextFile"};
-        String[] musikMass = { "A_music_shop", "Guitar", "Musical_instrument", "Piano", "Trubalzuya"};
         System.out.println("Ввод данных в коллекцию,значения данных берем с массива: ");
         for (String flowersMas : flowersMass) System.out.print(flowersMas+"\t");
         System.out.println("\n Содержание коллекции:");
--- src/M8/DZ2/SortedList.java
+++ src/M8/DZ2/SortedList.java
@@ -5,15 +5,13 @@ import java.util.TreeSet;
 public class SortedList {
 
     public static void main(String[] args) {
-        TreeSet<Comp> list =  new TreeSet<> ();
-        list.add(new Comp("Rosebush"));
-        list.add(new Comp("Tultip"));
-        list.add(new Comp("Chamomile"));
-        list.add(new Comp("Rose"));
-        list.add(new Comp("Aster"));
-        list.add(new Comp("Bouquet"));
-        for(Comp e : list) {
-            System.out.println(e.str);
-            }
+        TreeSet<String> list =  new TreeSet<> ();
+        list.add("Rosebush");
+        list.add("Tultip");
+        list.add("Chamomile");
+        list.add("Rose");
+        list.add("Aster");
+        list.add("Bouquet");
+        System.out.println(list);
         }
-    }
+}
